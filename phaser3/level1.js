a4c8626995961dc02f73d01a8311e62fec0a4f26
angelator312 <angel.raev2012@gmail.com>
2022-09-16 19:50:39 +0300

    ph
    h

--- phaser3/level1.js
+++ phaser3/level1.js
@@ -1,7 +1,7 @@
 import Level from "./level.js";
 class Level1  extends Level{
     static brplayr=2
-    static endscore=0
+    static endscore=100
     constructor() {
         super('level1',1)
     }
