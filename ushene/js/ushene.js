ae8407369f8bfb28c33b1d0bc9232c341601e54f
angelator312 <angel.raev2012@gmail.com>
2022-09-23 19:29:44 +0300

    usha

--- ushene/js/ushene.js
+++ ushene/js/ushene.js
@@ -4,11 +4,23 @@ let yorn = document.getElementById('yorn')
 function random(min=0,max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
+input.addEventListener("keypress", function(event) {
+    // If the user presses the "Enter" key on the keyboard
+    if (event.key === "Enter") {
+      // Cancel the default action, if needed
+      event.preventDefault();
+      // Trigger the button element with a click
+      document.getElementById("okbtn").click();
+    }
+  });
 function newtask() {
     let random1 =random(42,45)
     let znak = random1==44?'/':String.fromCharCode(random1)
-    if (znak=='/'|| znak=='-' || znak=='*') {
+    if (znak!='+') {
     let random2= random(0,random1)
+    if (znak!='-') {
+        random2=random(0,10)
+    }
     task.innerHTML = `${random(0,100)}${znak}${random2}`;
     }else{
     task.innerHTML = `${random(0,100)}${znak}${random(0,100)}`;
