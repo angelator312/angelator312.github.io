881d87b3f35319568efeb659a5dd1ea4a6176541
angelator312 <angel.raev2012@gmail.com>
2022-10-17 18:23:25 +0300

    gallery

---
+++ galleryregistrycookie_pkray/index.js
@@ -0,0 +1,160 @@
+const {
+  srandom, Filestore
+} = require('./jsonwjs')
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const multer = require('multer');
+const cookieParser = require('cookie-parser');
+const mime = require('mime-types');
+const sharp = require('sharp')
+const app = express();
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'image')
+  },
+  filename: function (req, file, cb) {
+    proverka(file, cb)
+  }
+})
+const templatePath = path.join(__dirname, 'views');
+const upload = multer({ dest: 'image/', storage });
+const port = 8080
+const cookieage = 60000 * 30
+const session = new Filestore('session.json');
+const registry = new Filestore('registry.json');
+function proverka(file, cb) {
+  fs.stat(`image/${file.originalname}`, (err, stats) => {
+    if (!stats) {
+      cb(null, file.originalname)
+    } else {
+      let nexte = file.originalname.substring(file.originalname.lastIndexOf('.'))
+      let newname = file.originalname.substring(0, file.originalname.lastIndexOf('.')) + new Date().getTime() + nexte;
+      cb(null, newname)
+    }
+  })
+}
+function sendFile(path, res) {
+  fs.readFile(`image/${path}`, function (err, data) {
+    if (!err) {
+      sharp(data)
+        .composite([
+          { input: 'aletter.jpg', gravity: 'northwest' }
+        ])
+        .jpeg({ mozjpeg: true })
+        .toBuffer((err, data, info) => {
+          if (err) {
+            console.error(err);
+          }
+          res.writeHead(200, { 'Content-Type': `image/jpeg` });
+          res.write(data);
+          return res.end();
+        });
+    } else {
+      res.writeHead(404, { 'Content-Type': `text/html; charset=utf-8` });
+      res.write('<h1>404</h1>');
+      res.write('няма такъв файл no such file');
+      return res.end();
+    }
+
+  });
+}
+app.engine('.html', require('ejs').__express);
+// конфигурираме къде се намират файловете с темплейти
+app.set('views', templatePath);
+app.use(cookieParser());
+// Конфигурираме  разширението по подразбиране за темплейтите
+app.set('view engine', '.html');
+app.get('/', function (req, res) {
+  const imageList = fs.readdirSync(`${process.cwd()}/image`).filter(t => t.endsWith('.jpg') || t.endsWith('.png'))
+  if (req.cookies.lognat) {
+    session.getkey(req.cookies.lognat, (err, data) => {
+      res.render('index', {
+        iList: imageList,
+        username: data.username
+      });
+    }
+    )
+  } else {
+    res.render('index', {
+      iList: imageList,
+      username: null
+    })
+  }
+
+})
+app.get('/registyr', function (req, res) {
+  res.render('registry', {
+    yorn: null
+  })
+})
+app.get('/registyrregistyr', function (req, res) {
+  if (req.query.password == req.query.password2) {
+    let sid = srandom()
+    registry.addkey(req.query.username, {
+      username: req.query.username,
+      password: req.query.password
+    },
+      (err) => {
+        res.cookie('lognat', sid, { maxAge: cookieage })
+        session.addkey(sid, {
+          username: req.query.username,
+          password: req.query.password
+        }, () => res.redirect('/'))
+      }
+    )
+  }
+})
+app.get('/login', function (req, res) {
+  res.render('login', {
+    yorn: ''
+  })
+});
+app.get('/logout', function (req, res) {
+  session.deletekey(req.cookies.lognat, () => {
+    res.clearCookie('lognat');
+    res.redirect('/');
+  })
+
+})
+app.get('/loginregister2', function (req, res) {
+  let valid = false;
+  let username = req.query.username
+  let password = req.query.password
+  registry.getkey(username, (err, data) => {
+    if (err) {
+      res.render('login', {
+        yorn: 'има грешка в username или в password'
+      })
+      return;
+    }
+    if (data.username == username && data.password == password) {
+      valid = true;
+    }
+    if (valid) {
+      let sid = srandom()
+      session.addkey(sid, { username, password }, () => {
+        res.cookie('lognat', sid, { maxAge: cookieage })
+        res.redirect('/')
+        return;
+      }
+      )
+    }
+  })
+})
+app.get('/upload', function (req, res) {
+  res.render('upload', {
+  })
+});
+app.post('/uploadimage', upload.single('myfile'), function (req, res, next) {
+  // req.file is the `avatar` file
+  // req.body will hold the text fields, if there were any
+  res.redirect('/')
+})
+
+app.get('/:path?', function (req, res) {
+  sendFile(req.params.path, res)
+});
+app.listen(port, () => {
+
+});
---
+++ galleryregistrycookie_pkray/jsonwjs.js
@@ -0,0 +1,60 @@
+const fs = require('fs');
+function srandom() {
+    let strg = '';
+    for (let index = 0; index < 22; index++) {
+        strg += Math.floor(Math.random() * 10)
+    }
+    return strg
+}
+class Filestore {
+    constructor(file) {
+        this.file = file
+    }
+    write(obj, callback) {
+        fs.writeFile(this.file, JSON.stringify(obj, null, 2), function (err) {
+            if (err) {
+                console.log(err);
+            }
+            callback(err)
+        })
+    }
+    read(callback) {
+        fs.readFile(this.file, function (err, data) {
+            if (err) {
+                console.log(err)
+                callback(err)
+            } else {
+                callback(err, JSON.parse(data))
+            }
+        })
+    }
+    addkey(key, value, callback) {
+        this.read( (err, data) => {
+            data[key] = value;
+            this.write( data, callback)
+        })
+    }
+    deletekey(key, callback) {
+        this.read( (err, data) => {
+            delete data[key]
+            this.write( data, callback)
+        })
+    }
+    getkey(key, callback) {
+        this.read( (err, data) => {
+            if (err) {
+                callback(err)
+            } else {
+                if(data[key]){
+                callback(err, data[key])
+                }else{
+                    callback(new Error('JSONproblems'))
+                }
+            }
+        })
+    }
+}
+module.exports = {
+    Filestore,
+    srandom
+}
